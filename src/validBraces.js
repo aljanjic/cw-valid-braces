429c88d4a00b77b4c7b07f7b972b875b32f8296c
Aljosa Janjic <aljosa.janjic@ibm.com>
2023-07-06 10:36:29 +0200

    Practicitg kata

--- src/validBraces.js
+++ src/validBraces.js
@@ -1,23 +1,65 @@
 function validBraces(str) {
-  const pairOfBrackets = {
-    '(': ')',
-    '[': ']',
-    '{': '}',
-  };
 
-  let arrLeftBrackets = [];
-  for (let i = 0; i < str.length; i++) {
+  const braces = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}'
+  }
+// pairOfBrackets[arrLeftBrackets.pop()]
+  let leftBrackets = [];
+  for (let i = 0; i < str.length; i ++){
     let char = str[i];
-    if (Object.keys(pairOfBrackets).includes(char)) {
-      arrLeftBrackets.push(char);
+    if (Object.keys(braces).includes(char)){
+      leftBrackets.push(char)
     } else {
-      if (char !== pairOfBrackets[arrLeftBrackets.pop()]) {
+      if (char !== braces[leftBrackets.pop()]){
         return false;
-      }
+      } 
     }
   }
 
-  return !arrLeftBrackets.length;
+return !leftBrackets.length;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const pairOfBrackets = {
+  //   '(': ')',
+  //   '[': ']',
+  //   '{': '}',
+  // };
+
+  // let arrLeftBrackets = [];
+  // for (let i = 0; i < str.length; i++) {
+  //   let char = str[i];
+  //   if (Object.keys(pairOfBrackets).includes(char)) {
+  //     arrLeftBrackets.push(char);
+  //   } else {
+  //     if (char !== pairOfBrackets[arrLeftBrackets.pop()]) {
+  //       return false;
+  //     }
+  //   }
+  // }
+
+  // return !arrLeftBrackets.length;
 }
 
 module.exports = validBraces;
