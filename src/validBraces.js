475831ff5487f9fdd522c4255d51b1bbe04a5cae
Aljosa Janjic <aljosa.janjic@ibm.com>
2023-07-16 17:24:08 +0200

    Recreating previous solution

--- src/validBraces.js
+++ src/validBraces.js
@@ -1,34 +1,53 @@
 function validBraces(str) {
 
-  const braces = {
-    '(' : ')',
-    '[' : ']',
-    '{' : '}'
-  }
-// pairOfBrackets[arrLeftBrackets.pop()]
-  let leftBrackets = [];
-  for (let i = 0; i < str.length; i ++){
-    let char = str[i];
-    if (Object.keys(braces).includes(char)){
-      leftBrackets.push(char)
-    } else {
-      if (char !== braces[leftBrackets.pop()]){
-        return false;
-      } 
+
+
+const braces = {
+  '(': ')',
+  '[': ']',
+  '{': '}'
+}
+
+console.log('Braces test: ', braces[')'])
+
+const validBraces = [];
+
+for(let i = 0; i < str.length; i++){
+  if (Object.keys(braces).includes(str[i])) {
+    validBraces.push(str[i])
+    console.log('Right side: ', str[i], validBraces[str[i]])
+  } else {
+    if (str[i] !== braces[validBraces.pop()]){
+      return false
     }
   }
+}
 
-return !leftBrackets.length;
-
-
-
+return !validBraces.length;
 
 
 
 
 
+//   const braces = {
+//     '(' : ')',
+//     '[' : ']',
+//     '{' : '}'
+//   }
+// // pairOfBrackets[arrLeftBrackets.pop()]
+//   let leftBrackets = [];
+//   for (let i = 0; i < str.length; i ++){
+//     let char = str[i];
+//     if (Object.keys(braces).includes(char)){
+//       leftBrackets.push(char)
+//     } else {
+//       if (char !== braces[leftBrackets.pop()]){
+//         return false;
+//       } 
+//     }
+//   }
 
-
+// return !leftBrackets.length;
 
 
 
